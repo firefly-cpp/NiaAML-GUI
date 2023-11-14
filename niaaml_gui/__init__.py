66ebe77749caae77e9d202aeca51b92d6e0f380f
Iztok Fister Jr <iztok@iztok-jr-fister.eu>
2023-11-14 12:42:44 +0100

    Bump version to 0.2.1

--- niaaml_gui/__init__.py
+++ niaaml_gui/__init__.py
@@ -5,4 +5,4 @@ from niaaml_gui.process_window_data import ProcessWindowData
 from niaaml_gui.progress_bar import ProgressBar
 
 __all__ = ["widgets", "windows", "ProcessWindowData", "ProgressBar", "MainAppWindow"]
-__version__ = "0.1.13"
+__version__ = "0.2.1"
